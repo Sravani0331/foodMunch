54414a703be3150ecb4c5eae4411e42bc283bc3b
Sravani Mulagundla <sravani.mulagundla@syenainfosoft.com>
2025-10-28 12:21:46 +0530

    Updated code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+declare var bootstrap: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'foodMunch';
+  ngAfterViewInit(): void {
+    const links = document.querySelectorAll('.nav-link-close');
+    const nav = document.getElementById('navbarNavAltMarkup');
+
+    links.forEach(link => {
+      link.addEventListener('click', () => {
+        const bsCollapse = new bootstrap.Collapse(nav!, { toggle: false });
+        bsCollapse.hide();
+      });
+    });
+  }
 }
